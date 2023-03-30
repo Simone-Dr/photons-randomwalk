9acb8ce62cca740a0256d4ea28d557786f40326b
Simone-Dr <s6sidrys@uni-bonn.de>
2023-03-30 23:43:14 +0200

    simplified mesh rendering

--- src/main.rs
+++ src/main.rs
@@ -56,58 +56,34 @@ pub async fn run() {
      
     model_material.render_states.cull = Cull::Back;
     let model = Gm::new(Mesh::new(&context, &cpu_mesh), model_material);
-    let mut wireframe_material = PhysicalMaterial::new_opaque(
-        &context,
-        &CpuMaterial {
-            albedo: Color::new_opaque(50, 50, 50),
-            roughness: 0.7,
-            metallic: 0.8,
-            ..Default::default()
-        },
-    );
-    wireframe_material.render_states.cull = Cull::Back;
-    let mut cylinder = CpuMesh::cylinder(10);
-    cylinder
-        .transform(&Mat4::from_nonuniform_scale(1.0, 0.0001, 0.0001))
-        .unwrap();
-    let edges = Gm::new(
-        InstancedMesh::new(&context, &edge_transformations(&cpu_mesh), &cylinder),
-        wireframe_material.clone(),
-    );
+   
 
-    let mut sphere = CpuMesh::sphere(8);
-    sphere.transform(&Mat4::from_scale(0.01)).unwrap();
-    let vertices = Gm::new(
-        InstancedMesh::new(&context, &vertex_transformations(&cpu_mesh), &sphere),
-        wireframe_material,
-    );
+
 
     let ambient = AmbientLight::new(&context, 0.7, Color::WHITE);
     let directional0 = DirectionalLight::new(&context, 2.0, Color::WHITE, &vec3(-1.0, -1.0, -1.0));
     let directional1 = DirectionalLight::new(&context, 2.0, Color::WHITE, &vec3(1.0, 1.0, 1.0));
 
-    let mut rw = RwSegment::new(&context);
+    let mut rw = RwSegment::new(&context, Color {r: 235, g: 201, b: 52, a: 200,});
     
     // main loop
     window.render_loop(move |mut frame_input| {
-        let mut redraw = frame_input.first_frame;
-        redraw |= camera.set_viewport(frame_input.viewport);
-        redraw |= control.handle_events(&mut camera, &mut frame_input.events);
-        redraw = true;
+        camera.set_viewport(frame_input.viewport);
+        control.handle_events(&mut camera, &mut frame_input.events);
+        
         
         rw.next(5, 2);
         
         
-        if redraw { // if first frame, or changed viewport or event
-            frame_input
-                .screen()
-                .clear(ClearState::color_and_depth(1.0, 1.0, 1.0, 1.0, 1.0))
-                .render(
-                    &camera,
-                    model.into_iter().chain(&vertices).chain(&edges).chain(&rw.gm).chain(&rw.gm_sphere),
-                    &[&ambient, &directional0, &directional1],
-                );
-        }
+        frame_input
+            .screen()
+            .clear(ClearState::color_and_depth(1.0, 1.0, 1.0, 0.0, 1.0))
+            .render(
+                &camera,
+                model.into_iter().chain(&rw.gm).chain(&rw.gm_sphere),
+                &[&ambient, &directional0, &directional1],
+            );
+        
         
 
         FrameOutput {
@@ -116,55 +92,6 @@ pub async fn run() {
         }
     });
 }
-
-fn vertex_transformations(cpu_mesh: &CpuMesh) -> Instances {
-    Instances {
-        transformations: cpu_mesh
-            .positions
-            .to_f32()
-            .into_iter()
-            .map(|p| Mat4::from_translation(p))  //Transformationmatrix chich translates w/ p
-            .collect(),
-        ..Default::default()
-    }
-}
-
-fn edge_transformations(cpu_mesh: &CpuMesh) -> Instances {
-    let indices = cpu_mesh.indices.to_u32().unwrap();   // get indices of mesh (u32), three cont. indices define a triangle 
-    let positions = cpu_mesh.positions.to_f32();    // get positions. vector of (3*f32)
-    let mut transformations = Vec::new();   // transformation matrix. vector of (4 f32 x 4 f32)
-    let mut keys = Vec::new();      // keys (sorted pairs of indices)
-    for f in 0..indices.len() / 3 {        //iterate through one third of indices
-        let mut fun = |i1, i2| {
-            let key = if i1 < i2 { (i1, i2) } else { (i2, i1) }; // to check, wheater edge has been transformed before
-            if !keys.contains(&key) { 
-                keys.push(key);
-                let p1: Vec3 = positions[i1];
-                let p2: Vec3 = positions[i2];
-                transformations.push(
-                    Mat4::from_translation(p1)
-                        * Into::<Mat4>::into(Quat::from_arc( //finds quaternion representing roation from 1,0,0 to p2-p1
-                            vec3(1.0, 0.0, 0.0),
-                            (p2 - p1).normalize(),
-                            None,
-                        ))
-                        * Mat4::from_nonuniform_scale((p1 - p2).magnitude(), 1.0, 1.0), // Translation * Rotation * Scaling
-                );
-            }
-        };
-        let i1 = indices[3 * f] as usize;       //together i1, i2, i3 define a triangle
-        let i2 = indices[3 * f + 1] as usize;
-        let i3 = indices[3 * f + 2] as usize;
-        fun(i1, i2);
-        fun(i2, i3);
-        fun(i3, i1);
-    }
-    Instances {
-        transformations,
-        ..Default::default()
-    }
-}
-
 
 fn get_transformation(start_pos: Vec3, end_pos: Vec3) -> Matrix4<f32> {
     let transformation = Mat4::from_translation(start_pos)
@@ -187,27 +114,24 @@ struct RwSegment{
 }
 
 impl RwSegment {
-    fn new(context : &Context) -> Self{
+    fn new(context : &Context, col : Color) -> Self{
         let transformations:Vec<Mat4> = Vec::new();
         let inst = Instances {
             transformations,
             ..Default::default()
         };
         
-        let tmp_gm= Gm::new(
-        InstancedMesh::new(&(context.clone()), &inst , &CpuMesh::cylinder(16)),
-        PhysicalMaterial::new_transparent(
+        let physMat = PhysicalMaterial::new_transparent(
             &context,
             &CpuMaterial {
-                albedo: Color {
-                    r: 235,  
-                    g: 201,
-                    b: 52,
-                    a: 200,
-                },
+                albedo: col,
                 ..Default::default()
             },
-            ),
+            );
+        
+        let tmp_gm= Gm::new(
+        InstancedMesh::new(&(context.clone()), &inst , &CpuMesh::cylinder(16)),
+        physMat.clone(),
         );
         
         RwSegment{
@@ -218,19 +142,7 @@ impl RwSegment {
             steps: 0,
             gm_sphere: Gm::new(
         Mesh::new(&context, &CpuMesh::sphere(16)),
-        PhysicalMaterial::new_transparent(
-            &context,
-            &CpuMaterial {
-                albedo: Color {
-                    r: 0,
-                    g: 0,
-                    b: 255,
-                    a: 200,
-                },
-                ..Default::default()
-            },
-            ),
-            ),
+        physMat,),
         }
     }
     
